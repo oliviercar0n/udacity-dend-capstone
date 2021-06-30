962dc9de83f8c1763db583cdb28e7d8e414f59c3
Olivier <oliviercaron28@gmail.com>
2021-06-29 20:09:47 -0400

    submission

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# udacity-dend-capstone
+# Bixi Telemetry Data Warehouse
+### Data Engineering Capstone Project
+
+#### Project Summary
+The goal of this project is to build a data warehouse for Bixi, a Montreal-based bike-sharing service. The modeled data will then be used to analyze ride pattern and availability of bikes at every station, enabling Bixi to optimize replenishment activities.
+
+### How to Run 
+All pipeline execution steps are contained within the `capstone-project.ipynb` notebook. The AWS IAM and Redshit cluster configs should be populated in the `dwh.cfg` file prior to running.
--- sql_queries.py
+++ sql_queries.py
@@ -1,11 +1,11 @@
 create_trips_table_query = f"""
     drop table if exists trips;
     create table if not exists trips(
-        trip_id int8 not null
+        trip_id int8 not null primary key
         , start_date timestamp not null
         , end_date timestamp not null
         , duration_sec int
-        , is_member varchar
+        , is_member bool
         , start_station_id int8 not null
         , end_station_id int8 not null
     )
@@ -14,11 +14,11 @@ create_trips_table_query = f"""
 create_gbfs_table_query = f"""
     drop table if exists gbfs;
     create table if not exists gbfs(
-        station_id int8
+        station_id int8 primary key
         , is_charging bool 
-        , is_installed int8
-        , is_renting int8
-        , is_returning int8
+        , is_installed bool
+        , is_renting bool
+        , is_returning bool
         , last_reported int8
         , num_bikes_available int8
         , num_bikes_disabled int8
@@ -39,6 +39,18 @@ create_stations_table_query = f"""
     )
 """
 
+create_time_table_query = f"""
+    drop table if exists time;
+    create table if not exists time(
+        datetime timestamp not null primary key
+        , hour int
+        , month int
+        , day int
+        , day_of_week int,
+        , hour int
+    )
+"""
+
 copy_table_query = """
     copy {}
     from '{}'
@@ -47,10 +59,31 @@ copy_table_query = """
     format as {}
 """
 
+copy_time_query = """
+    select
+        datetime
+        , extract(year from datetime) as year
+        , extract(month from datetime) as year
+        , extract(day from datetime) as year
+        , extract(day_of_week from datetime) as year
+        , extract(hour from datetime) as year
+    from (
+        select distinct datetime
+        from (
+            select start_date as datetime from trips
+            union all
+            select end_date as datetime from trips
+            union all
+            select last_updated_dt as datetime from gbfs
+        )
+    )
+"""
+
 create_queries = [
     create_trips_table_query,
     create_stations_table_query,
-    create_gbfs_table_query
+    create_gbfs_table_query,
+    create_time_table_query
 ]
 
 check_row_count = """
